bbfacf23632b3874beb3ea449795963aa5e3813e
hoanghoa <40349029+HoangHoa99@users.noreply.github.com>
2020-11-26 22:55:21 +0700

    Update ProductImageController.java

--- src/main/java/com/example/store/api/ProductImageController.java
+++ src/main/java/com/example/store/api/ProductImageController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("product-images")
 public class ProductImageController {
