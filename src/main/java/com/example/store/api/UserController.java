8e374916f00fece01d4959ffa7533c7d92d854ec
hoanghoa <40349029+HoangHoa99@users.noreply.github.com>
2020-11-26 22:55:40 +0700

    Update UserController.java

--- src/main/java/com/example/store/api/UserController.java
+++ src/main/java/com/example/store/api/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("users")
 public class UserController {
