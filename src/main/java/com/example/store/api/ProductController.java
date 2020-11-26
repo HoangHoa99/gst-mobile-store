fb41fce5ad7a670a014225b8218086fb26dadedd
hoanghoa <40349029+HoangHoa99@users.noreply.github.com>
2020-11-27 00:17:23 +0700

    Update ProductController.java

--- src/main/java/com/example/store/api/ProductController.java
+++ src/main/java/com/example/store/api/ProductController.java
@@ -4,6 +4,8 @@ import com.example.store.dto.request.ProductRequest;
 import com.example.store.dto.response.ProductResponse;
 import com.example.store.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,7 +13,6 @@ import javax.validation.Valid;
 import java.io.FileNotFoundException;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("products")
 public class ProductController {
@@ -19,7 +20,7 @@ public class ProductController {
     ProductService productService;
 
     @PostMapping("add")
-    public ProductResponse addProduct(
+    public ResponseEntity<?> addProduct(
             @ModelAttribute("name") String name,
             @ModelAttribute("price") Integer price,
             @ModelAttribute("quantity") Integer quantity,
@@ -30,7 +31,8 @@ public class ProductController {
             @Valid @ModelAttribute("image") MultipartFile image
             ) throws FileNotFoundException {
 
-        return productService.addProduct(ProductRequest.builder()
+                return ResponseEntity.status(HttpStatus.OK).body(
+                    productService.addProduct(ProductRequest.builder()
                                         .name(name)
                                         .price(price)
                                         .quantity(quantity)
@@ -39,22 +41,24 @@ public class ProductController {
                                         .category(cate)
                                         .condition(condition)
                                         .image(image)
-                                        .build());
+                                        .build())
+                                        );
     }
 
     @DeleteMapping("delete/{id}")
-    public void deleteProduct(@Valid @PathVariable Integer id){
-        productService.deleteProductById(id);
+    public ResponseEntity<?> deleteProduct(@Valid @PathVariable Integer id){
+        productService.deleteProductById(id)
+        return ResponseEntity.status(HttpStatus.OK).body("delete successful");
     }
 
     @GetMapping
-    public List<ProductResponse> getProductList(){
-        return productService.getListProduct();
+    public ResponseEntity<?> getProductList(){
+        return ResponseEntity.status(HttpStatus.OK).body(productService.getListProduct());
     }
 
     @GetMapping("/{id}")
-    public ProductResponse getProductById(@Valid @PathVariable Integer id){
-        return productService.getProductDetail(id);
+    public ResponseEntity<?> getProductById(@Valid @PathVariable Integer id){
+        return ResponseEntity.status(HttpStatus.OK).body(productService.getProductDetail(id));
     }
 
 }
