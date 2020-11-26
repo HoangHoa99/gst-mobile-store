c6778530457d8ea1b8aae315daadafa95ad491a3
HoangHoa99 <hoangthaihoa2122@gmail.com>
2020-11-27 01:22:45 +0700

    aaa

--- src/main/java/com/example/store/config/WebConfig.java
+++ src/main/java/com/example/store/config/WebConfig.java
@@ -10,9 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @EnableWebSecurity
 @Configuration
 public class WebConfig extends WebSecurityConfigurerAdapter {
@@ -23,16 +20,6 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     UserService userService;
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer(){
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
-            }
-        };
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
